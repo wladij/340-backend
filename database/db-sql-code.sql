fe856247163e49acb02ff6a7cf468824ef841659
wladij <joew.candelario@gmail.com>
2026-01-24 15:52:22 -0500

    week2

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -27,3 +27,13 @@ CREATE TABLE IF NOT EXISTS public.inventory
 	classification_id integer NOT NULL,
 	CONSTRAINT inventory_pkey PRIMARY KEY (inv_id)
 );
+
+-- 4. Update the description of 'GM Hummer' to replace "small interiors" with "a huge interior"
+UPDATE inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_make = 'GM' AND inv_model = 'Hummer';
+
+-- 6. Update all records in the inventory table to add '/vehicles' to the middle of the path in inv_image and inv_thumbnail
+UPDATE inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
